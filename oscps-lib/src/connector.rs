6f8965f7481a07b7e6f97e9bb80c95457aae71ef
Bhargav <bhargav.akula1@gmail.com>
2024-06-25 15:58:32 -0500

    removing unncessary nested modules

--- oscps-lib/src/connector.rs
+++ oscps-lib/src/connector.rs
@@ -1,12 +1,11 @@
-pub mod connector{
-    //here we will have 2 connector structs
-        //- Mass Streams
-        //- Energy Streams
-    pub struct Mconnector{
-        m_conn_id: String
-    }
-
-    pub struct Econnector{
-        e_conn_id: String
-    }
+//here we will have 2 connector structs
+    //- Mass Streams
+    //- Energy Streams
+pub struct Mconnector{
+    m_conn_id: String
 }
+
+pub struct Econnector{
+    e_conn_id: String
+}
+
