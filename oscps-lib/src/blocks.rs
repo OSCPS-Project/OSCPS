2a0f0a66200de971ea7437b7de4e35d89f65a3d4
Bhargav <bhargav.akula1@gmail.com>
2024-08-08 20:12:26 -0400

    uncommenting code and added documentation comments for blocks.rs

--- oscps-lib/src/blocks.rs
+++ oscps-lib/src/blocks.rs
@@ -4,33 +4,51 @@
 //! implemented by various structs to represent different unit operations.
 //!
 //! For example, if a block is a simple mixer, then it will implement the
-//! MassBalance trait but not th
+//! MassBalance trait but not the EnergyBalance.
 //!
 
-// use crate::connector;
+use crate::connector;
 
-// trait MassBalance {
-//     fn overall_massive_balance() {}
-// }
+/// Trait for ensuring the overall mass balance is maintained in a flowsheet.
+///
+/// This trait can be implemented by any block that needs to ensure mass conservation.
+trait MassBalance {
+    fn overall_mass_balance() {}
+}
 
-// trait EnergyBalance {
-//     fn energy_balance(){}
-// }
+/// # EnergyBalance
+///
+/// This trait ensures that blocks in the flowsheet adhere to energy conservation principles.
+///
+/// This is useful for distinguishing between "dynamic" and "steady state" simulations.
+trait EnergyBalance {
+    fn energy_balance() {}
+}
 
-// trait ElementBalance {
-//     fn element_balance(){}
-// }
+/// # ElementBalance (also known as Atomic Balance)
+///
+/// This trait ensures atomic conservation, especially relevant in reactor simulations.
+///
+/// Similar to the EnergyBalance trait, this is useful for determining the nature of the simulation (dynamic or steady state).
+trait ElementBalance {
+    fn element_balance() {}
+}
 
-// pub struct Mixer{
-//     pub block_id: String,
-//     pub input_stream: Vec<connector::Mconnector>,
-//     pub output_stream: connector::Mconnector
-// }
+/// # Mixer Block
+/// 
+/// A block used for simple stream mixing operations.
+/// 
+/// This struct requires the implementation of both EnergyBalance and MassBalance traits to ensure proper conservation principles are followed.
+pub struct Mixer {
+    pub block_id: String,
+    pub input_stream: Vec<connector::Mconnector>,
+    pub output_stream: connector::Mconnector,
+}
 
-// impl MassBalance for Mixer{
+impl MassBalance for Mixer {
+    // Implement mass balance methods specific to Mixer here.
+}
 
-// }
-
-// impl EnergyBalance for Mixer{
-
-// }
+impl EnergyBalance for Mixer {
+    // Implement energy balance methods specific to Mixer here.
+}
