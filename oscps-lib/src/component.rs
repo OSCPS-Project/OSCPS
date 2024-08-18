7340443398e086e15e7edfe03b992adaeeb984c2
Bhargav <bhargav.akula1@gmail.com>
2024-08-17 20:06:17 -0400

    implemented basic functionality for the component.rs module using the external package pubchem.rs from crates.io

--- oscps-lib/src/component.rs
+++ oscps-lib/src/component.rs
@@ -1,76 +1,85 @@
 //! # Component
 
 extern crate uom;
+extern crate pubchem;
 
-use uom::si::f64::ThermodynamicTemperature;
+/// This will hold the list of chemicals used within the simulation
+struct ChemicalList {
+    chemical_list : Vec<pubchem::Compound>
+}
+
+
+impl ChemicalList{
+    /// constructor that will hold the list of compounds being used in simulation
+    pub fn new() -> ChemicalList {
+        return ChemicalList {
+            chemical_list : Vec::<pubchem::Compound>::new()
+        };
+    }
+}
 
 /// A struct to store information regarding the chemical properties of a particular substance.
+/// The "Chemical" struct is a wrapper for the pubchem::Compound object
 struct Chemical {
     /// The (PubChem)[https://pubchem.ncbi.nlm.nih.gov/] CID of a compound.
-    component_id: u64,
-    /// The IUPAC name of a compound
-    iupac_name: String,
-    /// The chemical formula of a compound
-    chemical_formula: String,
-    /// The chemical properties of a compound
-    properties: ChemicalProperties
+    pubchem_obj : pubchem::Compound,
+    properties : pubchem::model::rest::Properties
+}
+
+/// This enum will be used by the "Chemical" struct to create the pubchem::Compound obj based on
+/// either the chemical name or the pubchem id of the chemical
+enum ChemicalIdentifier {
+    PubchemID(u32),
+    CompoundName(String),
 }
 
 impl Chemical {
-    /// Create a Chemical struct
-    ///
-    /// TODO: Finish this documentation comment
-    ///
-    pub fn new(component_id: u64, iupac_name: &str, chemical_formula: &str, properties: ChemicalProperties) -> Chemical {
-        Chemical {
-            component_id,
-            iupac_name: iupac_name.to_string(),
-            chemical_formula: chemical_formula.to_string(),
-            properties: properties,
-        }
+    pub fn new(identifier : ChemicalIdentifier) -> Chemical {
+        let pubchem_chemical_object = match identifier {
+            ChemicalIdentifier::PubchemID(id) => pubchem::Compound::new(id),
+            ChemicalIdentifier::CompoundName(name) => pubchem::Compound::with_name(name.as_str()),
+            //_ => panic!("Need to provide a u32 integer or String to identify the compound!!!")
+        };
+
+        //getting the properties of the chemical
+        let chemical_properties = pubchem_chemical_object
+            .properties(&[pubchem::CompoundProperty::Title, pubchem::CompoundProperty::MolecularFormula, pubchem::CompoundProperty::CanonicalSMILES, pubchem::CompoundProperty::ExactMass])
+            .unwrap();
+
+        return Chemical {
+            pubchem_obj : pubchem_chemical_object,
+            properties : chemical_properties
+        };
     }
 }
 
-/// A struct for storing chemical properties of a chemical.
-///
-/// This struct allows OSCPS access to the data needed to predict the various
-/// physical properties of a substance using thermodynamic correlations, including melting and
-/// boiling point, heat capacity, solubility, and many other properites.
-struct ChemicalProperties {
-    /// The melting point of a substance at atmospheric pressure in Kelvin
-    normal_melting_point: ThermodynamicTemperature,
-    /// The normal boiling point of a substance at atmospheric pressure in Kelvin
-    normal_boiling_point: ThermodynamicTemperature,
-}
-
-impl ChemicalProperties {
-    /// Create a ChemicalProperties struct
-    ///
-    pub fn new(
-        normal_melting_point: ThermodynamicTemperature,
-        normal_boiling_point: ThermodynamicTemperature
-        ) -> ChemicalProperties {
-        ChemicalProperties {
-            normal_melting_point,
-            normal_boiling_point,
-        }
-    }
-}
 
 #[cfg(test)]
 mod component_tests {
     use super::*;
     use std::io;
-    use uom::si::thermodynamic_temperature::kelvin;
-
+    use pubchem;
+    use uom::si::mass::gram;
+    use uom::si::f64::{Mass};
+    
     #[test]
     fn test_chemical_properties_constructor() -> io::Result<()> {
-        // Test using water
-        let water_melting_point = ThermodynamicTemperature::new::<kelvin>(273.15);
-        let water_boiling_point = ThermodynamicTemperature::new::<kelvin>(373.15);
-        let water_properties = ChemicalProperties::new(water_melting_point, water_boiling_point);
-        assert_eq!(water_properties.normal_melting_point, water_melting_point);
-        assert_eq!(water_properties.normal_boiling_point, water_boiling_point);
-        Ok(()) 
+        // Test using water (assuming 1 mole)
+        let water_mass_one_mole = Mass::new::<gram>(18.02);
+        let water_chemical_obj = Chemical::new(ChemicalIdentifier::PubchemID(962));
+        let water_mass_from_obj = (water_chemical_obj.properties.exact_mass.unwrap().parse().expect("Invalid Number"));
+        let water_mass_from_obj_uom = Mass::new::<gram>(water_mass_from_obj);
+
+        let diff = water_mass_from_obj_uom - water_mass_one_mole;
+        let abs_diff = diff.abs();
+        
+        if(abs_diff < Mass::new::<gram>(0.1))
+        {
+            return Ok(());
+        }
+        else{
+            Err(io::Error::new(io::ErrorKind::InvalidInput, "Mass difference is too large"))
+        }
     }
+
 }
