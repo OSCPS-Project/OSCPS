878f84f7f1fde57704a287204320b7916f9be8da
Bhargav <bhargav.akula1@gmail.com>
2024-08-23 20:03:16 -0400

    updating components.rs with ability to get chemcial properties :). Still need to test code

--- oscps-lib/src/component.rs
+++ oscps-lib/src/component.rs
@@ -1,9 +1,13 @@
 //! # Component
 
-use uom::si;
-
 extern crate uom;
+use uom::si::f64::{ThermodynamicTemperature, Mass, MassDensity};
+use uom::si::thermodynamic_temperature::kelvin;
+use uom::si::mass::kilogram;
+use uom::si::mass_density::kilogram_per_cubic_meter;
+
 extern crate pubchem;
+use anyhow::{anyhow, Result};
 
 /// This will hold the list of chemicals used within the simulation
 pub struct ChemicalList {
@@ -36,55 +40,88 @@ pub enum ChemicalIdentifier {
 }
 
 impl Chemical {
-    pub fn new(identifier : ChemicalIdentifier) -> Chemical {
+
+    /// constructor
+    pub async fn new(identifier : ChemicalIdentifier) -> Result<Self> {
         let pubchem_chemical_object = match identifier {
             ChemicalIdentifier::PubchemID(id) => pubchem::Compound::new(id),
             ChemicalIdentifier::CompoundName(name) => pubchem::Compound::with_name(name.as_str()),
         };
 
+        let cid_vec = pubchem_chemical_object.cids().unwrap();
+
+        let cid : i32 = cid_vec[0];
+
         //getting the properties of the chemical
-        let chemical_properties = pubchem_chemical_object
-            .properties(&[pubchem::CompoundProperty::Title, pubchem::CompoundProperty::MolecularFormula, pubchem::CompoundProperty::CanonicalSMILES, pubchem::CompoundProperty::ExactMass])
-            .unwrap();
+        let prop = ChemicalProperties::new(cid).await?;
 
-        return Chemical {
+        return Ok(Chemical {
             pubchem_obj : pubchem_chemical_object,
-            properties : chemical_properties
-        };
+            properties : prop
+        });
+    }
+    /// returns the pubchem object for the compound
+    pub fn get_pubchem_obj(&self) -> &pubchem::Compound {
+        return &self.pubchem_obj;
     }
 
-    pub fn get_pubchem_obj(&self) -> pubchem::Compound {
-        return self.pubchem_obj;
-    }
-
-    pub fn get_properties(&self) -> ChemicalProperties {
-        return self.properties;
-    }
-}
-
-impl Clone for Chemical {
-    fn clone(&self) -> Chemical {
-        return Chemical {
-            pubchem_obj : self.pubchem_obj,
-            properties : self.properties
-        }
+    /// returns the "ChemicalProperties" object for the "Chemical" object 
+    pub fn get_properties(&self) -> &ChemicalProperties {
+        return &self.properties;
     }
 }
 
 struct ChemicalProperties {
-    pub melting_pt : uom::si::f64::ThermodynamicTemperature,
-    pub boiling_pt : uom::si::f64::ThermodynamicTemperature,
-    pub density : uom::si::f64::MassDensity,
+    pub melting_pt : Option<uom::si::f64::ThermodynamicTemperature>,
+    pub boiling_pt : Option<uom::si::f64::ThermodynamicTemperature>,
+    pub density : Option<uom::si::f64::MassDensity>,
+    pub molec_mass : Option<uom::si::f64::Mass>
 }
 
 impl ChemicalProperties {
-    pub fn new() -> ChemicalProperties {
+    pub async fn new(cid : i32) -> Result<Self> {
+        let url = format!("https://pubchem.ncbi.nlm.nih.gov/rest/pug/compound/cid/{}/property/MolecularWeight,MeltingPoint,BoilingPoint,Density/JSON",cid);
+        let response: serde_json::Value = reqwest::get(&url).await?.json().await?;
 
+        // Extract the first (and typically only) set of properties
+        let properties_array = response["PropertyTable"]["Properties"].as_array();
+
+        if let Some(properties) = properties_array.and_then(|arr| arr.get(0)) { 
+        // Parse and convert the properties
+        let molec_mass = properties["MolecularWeight"]
+            .as_str()
+            .and_then(|w| w.parse::<f64>().ok())
+            .map(|w| uom::si::f64::Mass::new::<kilogram>(w / 1000.0));  // Converting molecular weight to grams
+
+        let melting_pt = properties["MeltingPoint"]
+            .as_str()
+            .and_then(|p| p.parse::<f64>().ok())
+            .map(|p| uom::si::f64::ThermodynamicTemperature::new::<kelvin>(p + 273.15));
+
+        let boiling_pt = properties["BoilingPoint"]
+            .as_str()
+            .and_then(|p| p.parse::<f64>().ok())
+            .map(|p| uom::si::f64::ThermodynamicTemperature::new::<kelvin>(p + 273.15));
+
+        let density = properties["Density"]
+            .as_str()
+            .and_then(|d| d.parse::<f64>().ok())
+            .map(|d| uom::si::f64::MassDensity::new::<kilogram_per_cubic_meter>(d));  // Density in kg/m^3
+
+        Ok(ChemicalProperties {
+            melting_pt,
+            boiling_pt,
+            density,
+            molec_mass,
+            })
+        } else {
+            Err(anyhow!("Properties not found for CID {}", cid))
+        }
     }
 
-    pub fn convert_units(&self, initial_units : &dyn uom::si::Units<f64>, final_units : &dyn uom::si::Units<f64>) {
+    // pub fn convert_units(&self, initial_units : &dyn uom::si::Units<f64>, final_units : &dyn uom::si::Units<f64>) {
 
-    }
+    // }
 }
 
 
