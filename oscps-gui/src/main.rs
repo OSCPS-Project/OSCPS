9cbc539d78f0e826c1b2cb177e64a82a6f8c12d2
Nathaniel Thomas <nathaniel@swbell.net>
2024-12-17 08:46:03 -0600

    primitive example

--- oscps-gui/src/main.rs
+++ oscps-gui/src/main.rs
@@ -1,88 +1,202 @@
-//! This example showcases an interactive `Canvas` for drawing Bézier curves.
-mod bezier;
-use iced::widget::{button, container, horizontal_space, hover, column};
-use iced::{Element, Fill, Theme};
+mod flowsheet;
+mod style;
 
-// Main function. Returns result from iced::application
+use iced::keyboard;
+use iced::widget::pane_grid::{self, PaneGrid};
+use iced::widget::{button, column, container, horizontal_space, hover, responsive, row, scrollable, text};
+use iced::{Center, Color, Element, Fill, Size, Subscription, Theme};
+
 pub fn main() -> iced::Result {
-    // Sets window title, update method, and view method. Sets the theme, enables
-    // antialiasing, centers the main window, and runs the program.
-    println!("Starting application...");
-    iced::application("Bezier Tool - Iced", Example::update, Example::view)
+    iced::application("Open Source Chemical Process Simulator", MainWindow::update, MainWindow::view)
         .theme(|_| Theme::Dark)
         .antialiasing(true)
         .centered()
         .run()
 }
 
-// Creates the Example struct, which has a bezier and and a vector of beziers.
-// Presumably, the first bezier is the one actively being used
-#[derive(Default)]
-struct Example {
-    bezier: bezier::State, // State variable, stores cache, which stores geometry.
-    curves: Vec<bezier::Curve>, // Curves are just three points.
+struct MainWindow {
+    panes: pane_grid::State<Pane>,
+    panes_created: usize,
+    focus: Option<pane_grid::Pane>,
+    flowsheet: flowsheet::State,
+    curves: Vec<flowsheet::Curve>,
 }
 
-// Messages that are processed. The first is a click to add a curve, the second
-// is to clear the screen.
 #[derive(Debug, Clone, Copy)]
 enum Message {
-    AddCurve(bezier::Curve),
+    AddCurve(flowsheet::Curve),
     Clear,
-    Test,
+    PlaceMixer,
+    Split(pane_grid::Axis, pane_grid::Pane),
+    SplitFocused(pane_grid::Axis),
+    FocusAdjacent(pane_grid::Direction),
+    Clicked(pane_grid::Pane),
+    Dragged(pane_grid::DragEvent),
+    Resized(pane_grid::ResizeEvent),
+    TogglePin(pane_grid::Pane),
+    Maximize(pane_grid::Pane),
+    Restore,
+    Close(pane_grid::Pane),
+    CloseFocused,
 }
 
-// For example, we are going to implement the update and view functions.
-impl Example {
-    // This function accepts the Example struct, as well as a message.
-    fn update(&mut self, message: Message) {
-        // Match the message to one of two actions
-        match message {
-            // If it is add curve, we will push a curve to the curves struct.
-            // The message will also contain the curve to add. We will then
-            // redraw the screen.
-            Message::AddCurve(curve) => {
-                self.curves.push(curve);
-                self.bezier.request_redraw();
-            }
-            // If a clear message is sent, then the state of the bezier will
-            // be set to default, and the bezier curve vector will be cleared.
-            Message::Clear => {
-                self.bezier = bezier::State::default();
-                self.curves.clear();
-            }
-            Message::Test => {
-                println!("Test clicked!");
-            }
+impl MainWindow {
+
+    fn new() -> Self {
+        let (mut panes, pane) = pane_grid::State::new(Pane::new_canvas(0));
+        panes.split(pane_grid::Axis::Horizontal, pane, Pane::new_selection());
+
+        MainWindow {
+            panes,
+            panes_created: 1,
+            focus: None,
+            flowsheet: flowsheet::State::default(),
+            curves: Vec::default(),
         }
     }
-    // For this function, we are going to configure how the view is seen by the
-    // user.
+
+    fn update(&mut self, message: Message) {
+        match message {
+            Message::AddCurve(curve) => {
+                self.curves.push(curve);
+                self.flowsheet.request_redraw();
+            }
+            Message::Clear => {
+                self.flowsheet = flowsheet::State::default();
+                self.curves.clear();
+            }
+            Message::PlaceMixer => {
+                println!("Placing Mixer!");
+            }
+            Message::Split(_axis, _pane) => {
+                println!("You split a pane!")
+            },
+            Message::SplitFocused(_axis) => {
+                println!("You split a focused pane!")
+            },
+            Message::FocusAdjacent(_direction) => (),
+            Message::Clicked(pane) => {
+                self.focus = Some(pane);
+                println!("You clicked on a pane!")
+                },
+            Message::Dragged(pane_grid::DragEvent::Dropped{ pane, target }) => { // pane, target
+                self.panes.drop(pane, target);
+                println!("You dragged a pane!")
+            },
+            Message::Dragged(_) => {
+                println!("You dragged, but did not drop a pane!")
+            },
+            Message::Resized(pane_grid::ResizeEvent { split, ratio } ) => {
+                self.panes.resize(split, ratio);
+                println!("You resized a pane!")
+            },
+            Message::TogglePin(_pane) => {
+                println!("You pinned a pane!")
+            },
+            Message::Maximize(_pane) => {
+                println!("You maximized a pane!")
+            },
+            Message::Restore => {
+                println!("You restored a window!")
+            },
+            Message::Close(_pane) => {
+                println!("You closed a pane!")
+            },
+            Message::CloseFocused => {
+                println!("You closed a focused pane!")
+            },
+            _ => ()
+        }
+    }
+
+    fn subscription(&self) -> Subscription<Message> {
+        keyboard::on_key_press(|key_code, modifiers| {
+            if !modifiers.command() {
+                return None;
+            }
+
+            handle_hotkey(key_code)
+        })
+    }
+
     fn view(&self) -> Element<Message> {
-        // A container (https://docs.rs/iced/latest/iced/widget/container/index.html)
-        // is essentially a box that allows a user to align the contents within. In 
-        // this case, we are giving it a padding of 10 (presumably points) and will
-        // aligh_right(Fill), which will align the container to the right. This should
-        // not effect the appearance, as the container has even padding on all sides. 
-        // Confirmed, there appears to be no effect.
-        container(
-            // Hover displays one widget on top of another one. This will be 
-            // where the bezier curves are drawn. (I think)
-            column![
+        let focus = self.focus;
+        let total_panes = self.panes.len();
+
+        let pane_grid = PaneGrid::new(&self.panes, |id, pane, _is_maximized| {
+        match pane {
+            Pane::Canvas{ id: _, is_pinned: _} => {
+                println!("Found canvas!"); 
+            }
+            Pane::UnitSelection => {
+                println!("Found Selection!");
+                return row![
+                    container(
+                        button("Place Mixer").on_press(Message::PlaceMixer),
+                    ),
+                    container(
+                        button("Place Other Component"),
+                    ),
+                ].into()
+            }
+        }
+        let is_focused = focus == Some(id);
+
+        //  let pin_button = button(
+        //     text(if pane.is_pinned { "Unpin" } else { "Pin" }).size(14),
+        //  )
+        // .on_press(Message::TogglePin(id))
+        // .padding(3);
+
+        let title = row![
+            // pin_button,
+            "Pane",
+            // text(pane.id.to_string()).color(if is_focused{
+            //     PANE_ID_COLOR_FOCUSED
+            // } else {
+            //     PANE_ID_COLOR_UNFOCUSED
+            // }),
+        ]
+            .spacing(5);
+
+            let title_bar = pane_grid::TitleBar::new(title)
+                // .controls(pane_grid::Controls::dynamic(
+                //         view_controls(
+                //             id,
+                //             total_panes,
+                //             pane.is_pinned,
+                //             is_maximized,
+                //         ),
+                //         button(text("X").size(14))
+                //             .style(button::danger)
+                //             .padding(3)
+                //             .on_press_maybe(
+                //                 if total_panes > 1 && !pane.is_pinned {
+                //                     Some(Message::Close(id))
+                //                 } else {
+                //                     None
+                //                 },
+                //             ),
+                // ))
+                .padding(10)
+                .style(if is_focused {
+                    style::title_bar_focused
+                } else {
+                    style::title_bar_active
+                });
+
+            pane_grid::Content::new(responsive(move |size| {
+                view_content(
+                id, 
+                total_panes, 
+                false,
+                // pane.is_pinned,
+                size,
             hover(
-                // This askes the current bezier curve to display a view, and it 
-                // will map it to the AddCurve message.
-                self.bezier.view(&self.curves).map(Message::AddCurve),
-                // If the curves vector is empty, just draw a container with a horizontal space
-                // inside.
+                self.flowsheet.view(&self.curves).map(Message::AddCurve),
                 if self.curves.is_empty() {
                     container(horizontal_space())
                 } else {
-                    // What we are doing to do is return a button which allows
-                    // the user to clear the screen. In this case it will have the
-                    // "danger" style, which makes it red. It will then clear the
-                    // screen if it is pressed. It is again given  padding and aligned right. 
-                    // In this case
                     container(
                         button("Clear")
                         .style(button::danger)
@@ -92,13 +206,185 @@ impl Example {
                     .align_top(Fill)
                 },
             ),
-            container(
-                button("Test text").on_press(Message::Test),
-            )
 
+                )
+        }))
+        .title_bar(title_bar)
+        .style(if is_focused {
+            style::pane_focused
+        } else {
+            style::pane_active
+        })
+        })
+        .width(Fill)
+        .height(Fill)
+        .spacing(10)
+        .on_click(Message::Clicked)
+        .on_drag(Message::Dragged)
+        .on_resize(10, Message::Resized);
+
+        container(
+            column![
+            pane_grid,
             ]
         )
         .padding(20)
         .into()
     }
+}
+
+impl Default for MainWindow {
+    fn default() -> Self {
+        MainWindow::new()
+    }
+}
+
+const PANE_ID_COLOR_UNFOCUSED: Color = Color::from_rgb(
+    0xFF as f32 / 255.0,
+    0xC7 as f32 / 255.0,
+    0xC7 as f32 / 255.0,
+);
+
+const PANE_ID_COLOR_FOCUSED: Color = Color::from_rgb(
+    0xFF as f32 / 255.0,
+    0x47 as f32 / 255.0,
+    0x47 as f32 / 255.0,
+);
+
+fn handle_hotkey(key: keyboard::Key) -> Option<Message> {
+    use keyboard::key::{self, Key};
+    use pane_grid::{Axis, Direction};
+
+    match key.as_ref() {
+        Key::Character("v") => Some(Message::SplitFocused(Axis::Vertical)),
+        Key::Character("h") => Some(Message::SplitFocused(Axis::Horizontal)),
+        Key::Character("w") => Some(Message::CloseFocused),
+        Key::Named(key) => {
+            let direction = match key {
+                key::Named::ArrowUp => Some(Direction::Up),
+                key::Named::ArrowDown => Some(Direction::Down),
+                key::Named::ArrowLeft => Some(Direction::Left),
+                key::Named::ArrowRight => Some(Direction::Right),
+                _ => None,
+            };
+
+            direction.map(Message::FocusAdjacent)
+        }
+        _ => None,
+    }
+}
+
+// #[derive(Clone,Copy,Default)]
+// struct Pane {
+//     id: usize,
+//     pub is_pinned: bool,
+// }
+#[derive(Clone,Copy,Default)]
+enum Pane {
+    Canvas{
+        id: usize,
+        is_pinned: bool,
+    },
+    #[default]
+    UnitSelection,
+}
+
+impl Pane {
+    fn new_canvas(id: usize) -> Self {
+        Pane::Canvas {
+            id,
+            is_pinned: false,
+        }
+    }
+
+    fn new_selection() -> Self {
+        Pane::UnitSelection
+    }
+
+    // fn default() -> Self {
+    //     Pane::Canvas {
+    //         id: 0,
+    //         is_pinned: false,
+    //     }
+    // }
+}
+
+
+fn view_content<'a>(
+    _pane: pane_grid::Pane,
+    _total_panes: usize,
+    _is_pinned: bool,
+    size: Size,
+    flowsheet: Element<'a, Message>,
+) -> Element<'a, Message> {
+    // let button = |label, message| {
+    //     button(text(label).width(Fill).align_x(Center).size(16))
+    //         .width(Fill)
+    //         .padding(8)
+    //         .on_press(message)
+    // };
+
+    // let controls = column![
+    //     button(
+    //         "Split horizontally",
+    //         Message::Split(pane_grid::Axis::Horizontal, pane),
+    //     ),
+    //     button(
+    //         "Split vertically",
+    //         Message::Split(pane_grid::Axis::Vertical, pane),
+    //     )
+    // ]
+    // .push_maybe(if total_panes > 1 && !is_pinned {
+    //     Some(button("Close", Message::Close(pane)).style(button::danger))
+    // } else {
+    //     None
+    // })
+    // .spacing(5)
+    // .max_width(160);
+
+    let content =
+        column![flowsheet, text!("{}x{}", size.width, size.height).size(24), ] // controls,
+            .spacing(10)
+            .align_x(Center);
+
+    // container(scrollable(content))
+    container(content)
+        .center_y(Fill)
+        .padding(5)
+        .into()
+}
+
+fn view_controls<'a>(
+    pane: pane_grid::Pane,
+    total_panes: usize,
+    is_pinned: bool,
+    is_maximized: bool,
+) -> Element<'a, Message> {
+    let row = row![].spacing(5).push_maybe(if total_panes > 1 {
+        let (content, message) = if is_maximized {
+            ("Restore", Message::Restore)
+        } else {
+            ("Maximize", Message::Maximize(pane))
+        };
+
+        Some(
+            button(text(content).size(14))
+                .style(button::secondary)
+                .padding(3)
+                .on_press(message),
+        )
+    } else {
+        None
+    });
+
+    let close = button(text("Close").size(14))
+        .style(button::danger)
+        .padding(3)
+        .on_press_maybe(if total_panes > 1 && !is_pinned {
+            Some(Message::Close(pane))
+        } else {
+            None
+        });
+
+    row.push(close).into()
 }
---
+++ oscps-gui/src/style.rs
@@ -0,0 +1,50 @@
+use iced::widget::container;
+use iced::{Border, Theme};
+
+pub fn title_bar_active(theme: &Theme) -> container::Style {
+    let palette = theme.extended_palette();
+
+    container::Style {
+        text_color: Some(palette.background.strong.text),
+        background: Some(palette.background.strong.color.into()),
+        ..Default::default()
+    }
+}
+
+pub fn title_bar_focused(theme: &Theme) -> container::Style {
+    let palette = theme.extended_palette();
+
+    container::Style {
+        text_color: Some(palette.primary.strong.text),
+        background: Some(palette.primary.strong.color.into()),
+        ..Default::default()
+    }
+}
+
+pub fn pane_active(theme: &Theme) -> container::Style {
+    let palette = theme.extended_palette();
+
+    container::Style {
+        background: Some(palette.background.weak.color.into()),
+        border: Border {
+            width: 2.0,
+            color: palette.background.strong.color,
+            ..Border::default()
+        },
+        ..Default::default()
+    }
+}
+
+pub fn pane_focused(theme: &Theme) -> container::Style {
+    let palette = theme.extended_palette();
+
+    container::Style {
+        background: Some(palette.background.weak.color.into()),
+        border: Border {
+            width: 2.0,
+            color: palette.primary.strong.color,
+            ..Border::default()
+        },
+        ..Default::default()
+    }
+}
