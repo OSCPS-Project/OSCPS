baa861881413379f94af955ad0e42b4e10ef302d
Bhargav Akula <mail.bhargavakula@gmail.com>
2025-04-03 21:00:39 -0400

    adding properties of the 'PureSpeciesProperties' struct

--- oscps-lib/src/properties/pure_species_properties.rs
+++ oscps-lib/src/properties/pure_species_properties.rs
@@ -6,10 +6,20 @@
 
 
 use crate::properties::*;
+use uom::si::f64;
+use std::sync::Arc;
+use oscps_db::*;
 
 ///#PureSpeciesProperties
 ///
 ///This will contain all the important properties for pure species
 pub struct PureSpeciesProperties {
-
+    pub species_obj_id: Arc<f64>,
+    pub antoine_equation_constants: Vec<f64::Ratio>,
+    pub critical_temperature: f64::ThermodynamicTemperature,
+    pub molar_mass: f64::MolarMass,
+    pub normal_boiling_point: f64::ThermodynamicTemperature,
+    pub critical_molar_volume: f64::Volume,
+    pub accentric_factor: f64::Ratio,
+    pub compressibility_factor: f64::Ratio
 }
